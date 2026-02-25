04fc255230b0fbdfd4dae7277e7d2a7742a0aefd
Ethan Ho <batchfile@gmail.com>
2026-02-25 15:41:11 -0500

    added parameter handling

--- sshs.sh
+++ sshs.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 
+NUM_OF_ARGS="$#"
+MODE=""
+
+invalid() {
+	echo "Usage error. $1"
+	exit 1
+}
+
+if [ ! "$NUM_OF_ARGS" -eq 2 ]; then
+	invalid "Invalid number of arguments.
+fi
+
+if [ "$1" = "-c" ]; then
+	MODE="CREATE"
+elif [ "$1" = "-d" ]; then
+	MODE="DELETE"
+elif [ "$1" = "-a" ]; then
+	MODE="ATTACH"
+else
+	invalid "Invalid mode."
+fi
+
+
